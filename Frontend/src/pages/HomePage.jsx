192301ca5ce763e3017e82984867b57e8b0fc7da
Ritik Gwala <ritikgwala6397@gmail.com>
2024-09-26 22:33:15 +0530

    test: remove env & fix in FE

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import LeftSide from '../components/LeftSide'
-import MiddleSide from '../components/MiddleSide'
-import RighSide from '../components/RighSide'
 const HomePage = () => {
   return (
     <div>HomePage</div>
